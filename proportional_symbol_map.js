4c21785af54fdfc4b669d449836b5297cb89e098
Foo Kai Yan <110818481+Wh1teF0X2004@users.noreply.github.com>
2024-09-21 23:47:59 +0800

    Update proportional_symbol_map.js

--- proportional_symbol_map.js
+++ proportional_symbol_map.js
@@ -1,4 +1,4 @@
-var proportional_symbol_map_1 = "proportional_symbol_map.vg.json";
+var proportional_symbol_map_1 = "proportional_symbol_map.json";
 
 vegaEmbed("#proportional_symbol_map", proportional_symbol_map_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
